fa18ce36657cde05e863417269850e1f87b4fde8
fcicerelli <fcicerellitic@gmail.com>
2025-02-04 17:21:23 -0300

    delete task complete

--- components/addTask.js
+++ components/addTask.js
@@ -19,10 +19,14 @@ export const addTask = (evento) => {
   
   input.value = '';
   calendar.value = '';
+
+  const complete = false;
  
   const taskObj = {
     value,
-    dateFormat
+    dateFormat,
+    complete,
+    id: uuid.v4(),
   }
 
   list.innerHTML = "";
@@ -35,23 +39,31 @@ export const addTask = (evento) => {
   displayTasks();
 }
 
-export const createTask = ({value, dateFormat}) => {
+export const createTask = ({value, dateFormat, complete, id}) => {
   const task = document.createElement('li');
         task.classList.add('card');
   
-  const taskContent = document.createElement('div');
+        const taskContent = document.createElement('div');
+
+  const check = checkComplete(id);
+
+  if(complete) {
+    check.classList.toggle('fas');
+    check.classList.toggle('completeIcon');
+    check.classList.toggle('far');
+  }
   
   const titleTask = document.createElement('span');
         titleTask.classList.add('task');
         titleTask.innerText = value;
-        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(check);
         taskContent.appendChild(titleTask);
  
   const dateElement = document.createElement('span');
         dateElement.innerHTML = dateFormat;
         task.appendChild(taskContent);
         task.appendChild(dateElement);
-        task.appendChild(deleteIcon());
+        task.appendChild(deleteIcon(id));
 
   return task;
 };
